6e81450efc21750bb7e42c3b1b91e7e73b236c72
ypsilon.takai <ypsilon.takai@gmail.com>
2013-11-14 19:26:38 +0900

    first step

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj_advent_2013_math "0.0.1"
+  :description "article and sample codes for 2013 clojure advent calender Japan."
+  :url "http://connpass.com/event/3991/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.2"]])
---
+++ src/clj_advent_2013_math/core.clj
@@ -0,0 +1,6 @@
+(ns clj-advent-2013-math.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
